cc7d27393e00e1f9b3e06d42e1148ac04b21e77a
Divyanshu <divyanshutiwari173@gmail.com>
2024-09-05 22:14:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('Hello');
+
+const showNotificationDiv = document.querySelector('.notification');
+const successMsg = '<i class="fa-solid fa-circle-check"></i> Successfully Submitted';
+const errorMsg = '<i class="fa-solid fa-circle-exclamation"></i> Error Occured';
+const invalidMsg = '<i class="fa-solid fa-circle-xmark"></i> Invalid Input';
+
+
+function showNotification(msg) {
+
+    let popUp = document.createElement('div');
+
+    popUp.classList.add('pop');
+    popUp.innerHTML = msg;
+    showNotificationDiv.appendChild(popUp);
+
+    if (msg.includes('Error')) {
+        popUp.classList.add('error');
+    }
+    if (msg.includes('Invalid')) {
+        popUp.classList.add('invalid');
+    }
+
+    setTimeout(()=>{
+        popUp.remove();
+    },3500);
+}
